85b6c7378be1a9e9ee7caab8cf7cc20d87a50947
hijriah24 <hijriahijria74@gmail.com>
2025-12-11 19:49:35 +0800

    Delete UAS pemrograman directory

--- UAS pemrograman/contoh dictionary.py
+++
@@ -1,7 +0,0 @@
-mahasiswa = {
-    "nama": "Andi",
-    "nim": "D0421001",
-    "nilai": 90
-}
-
-print(mahasiswa["nama"])
--- UAS pemrograman/tipe data serng dipakai.py
+++
@@ -1,5 +0,0 @@
-nama = "hijriah" # string
-umur = 20 # integer
-tinggi = 160.2 # float
-
-
