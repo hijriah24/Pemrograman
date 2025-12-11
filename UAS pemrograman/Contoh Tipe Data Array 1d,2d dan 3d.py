d719552ede58954fea5f642185cbf97f735438f9
hijriah24 <hijriahijria74@gmail.com>
2025-12-11 09:43:16 +0800

    Create Contoh Tipe Data Array 1d,2d dan 3d.py

---
+++ UAS pemrograman/Contoh Tipe Data Array 1d,2d dan 3d.py
@@ -0,0 +1,25 @@
+# Array 1 dimensi
+angka = [10, 20, 30, 40, 50]
+print(angka[2])  # Output: 3
+
+# Array 2 dimensi
+matriks = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(matriks[1][2])  # Output: 6
+
+
+# Array 3 dimensi
+array3d = [
+    [
+        [1, 2], 
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+]
+print(array3d[1][0][1])  # Output: 6
